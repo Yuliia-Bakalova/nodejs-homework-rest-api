893669dcc4f199ce3b3e7345f0a4d52c4cb6abde
Yuliia-Bakalova <98843051+Yuliia-Bakalova@users.noreply.github.com>
2023-02-06 16:22:39 +0200

    done hw4

---
+++ routes/api/users.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const {
+  register,
+  login,
+  current,
+  logout,
+  subscriptionStatusUpdate,
+} = require("../../controllers/user.controller");
+const { validateBody, auth } = require("../../middlewares");
+const { tryCatchWrapper } = require("../../helpers/index");
+const { loginSchema, registerSchema, updateSubscriptionSchema,} = require("../../schemas/user");
+ 
+const authRouter = express.Router();
+authRouter.post(
+  "/signup",
+  validateBody(registerSchema),
+  tryCatchWrapper(register)
+);
+authRouter.post("/login", validateBody(loginSchema), tryCatchWrapper(login));
+authRouter.get("/current", auth, tryCatchWrapper(current));
+authRouter.get("/logout", auth, tryCatchWrapper(logout));
+authRouter.patch(
+  "/",
+  auth,
+  validateBody(updateSubscriptionSchema),
+  tryCatchWrapper(subscriptionStatusUpdate)
+);
+
+module.exports = {
+  authRouter,
+};
