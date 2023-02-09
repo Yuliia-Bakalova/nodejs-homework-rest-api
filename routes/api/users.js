8acbfda38f8bf764d07c5167400be3eeaad48599
Yuliia-Bakalova <98843051+Yuliia-Bakalova@users.noreply.github.com>
2023-02-09 09:37:01 +0200

    hw5

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,26 +5,29 @@ const {
   current,
   logout,
   subscriptionUpdate,
+   updateAvatar
 } = require("../../controllers/user.controller");
-const { validateBody, auth } = require("../../middlewares");
+const { validateBody, auth, upload, resizeAvatar, } = require("../../middlewares");
 const { tryCatchWrapper } = require("../../helpers/index");
-const { loginSchema, registerSchema, updateSubscriptionSchema,} = require("../../schemas/user");
+const { loginSchema, registerSchema, updateSubscriptionSchema, } = require("../../schemas/user");
+
  
 const authRouter = express.Router();
-authRouter.post(
-  "/signup",
-  validateBody(registerSchema),
-  tryCatchWrapper(register)
-);
+
+authRouter.post(  "/signup", validateBody(registerSchema), tryCatchWrapper(register));
 authRouter.post("/login", validateBody(loginSchema), tryCatchWrapper(login));
 authRouter.get("/current", auth, tryCatchWrapper(current));
 authRouter.get("/logout", auth, tryCatchWrapper(logout));
+authRouter.patch( "/", auth, validateBody(updateSubscriptionSchema), tryCatchWrapper(subscriptionUpdate));
+
+
 authRouter.patch(
-  "/",
-  auth,
-  validateBody(updateSubscriptionSchema),
-  tryCatchWrapper(subscriptionUpdate)
-);
+  "/avatars", auth,upload.single("avatar"), tryCatchWrapper(resizeAvatar), tryCatchWrapper(updateAvatar));
+ 
+  
+ 
+ 
+
 
 module.exports = {
   authRouter,
